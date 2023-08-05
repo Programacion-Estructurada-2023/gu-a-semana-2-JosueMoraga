2abd1b0c3a6971833f5c5f49ec7091b725c84a6b
JosueMoraga <140348234+JosueMoraga@users.noreply.github.com>
2023-08-05 17:12:46 -0600

    U20230363_Semana2.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
@@ -9,12 +9,33 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
 
 
-            int Variable = 2;
+            //ITEMS 1.
+
+            string PrimerNombre= "Josue";
+            string SegundoNombre= "Benjamin";
+            string PrimerApellido= "Moraga";
+            string SegundoApellido= "Molina";
+
+            //ITEMS 2.
+
+            int EdadJosue= 21;
+            int EdadMauricio= 29;
+            int EdadSara= 22;
+
+            //ITEMS 3.
+
+            float ComidaPerro= 0.75;
+            float Comida= 200.00;
+            float Agua= 7.25;
+
+            //ITEMS 4.
+
+            bool EstudianteActivo= true;
+            bool EstudianteBecado= false;
+
+            //AUTOEVALUACION 10/10.
 
             
-            Console.Write("La variable ingresada fue :" + Variable);
-
-            Console.ReadKey();
 
 
         }
